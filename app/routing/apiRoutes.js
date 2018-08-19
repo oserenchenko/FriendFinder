1bb29f2dd6d52c3a5db3381ec66ba01a2bd65e2b
Olga <oserenchenko@gmail.com>
2018-08-19 11:24:23 -0600

    compatibility function finds friend match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,13 +4,39 @@ var path = require("path");
 //load friend data
 var friendsArrData = require("../data/friends");
 console.log(friendsArrData);
-function compatible(object) {
-  for (var i = 0; i < friendsArrData.length; i++) {
-    for (var j = 0; j < object.scores.length; j++) {
 
+function lowestValInArr(array) {
+  var index = 0;
+  var value = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] < value) {
+      value = array[i];
+      index = i;
     }
   }
+  return index;
 }
+
+function compatible(object) {
+  var totalDiffArr = [];
+  for (var i = 0; i < friendsArrData.length; i++) {
+    var totalDifference = 0;
+    for (var j = 0; j < object.scores.length; j++) {
+      var diff = object.scores[j] - friendsArrData[i].scores[j];
+      if (diff < 0) {
+        diff = diff * -1;
+      }
+      totalDifference += diff;
+    }
+    console.log(totalDifference);
+    totalDiffArr.push(totalDifference);
+  }
+  index = lowestValInArr(totalDiffArr);
+  var friendMatch = friendsArrData[index];
+  console.log(friendMatch);
+}
+
+
 
 module.exports = function (app) {
   //GET route to api/friends
