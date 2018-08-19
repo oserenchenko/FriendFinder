d0f1e378fa7282a569abc5d80981bb2eec18b1f6
Olga <oserenchenko@gmail.com>
2018-08-19 10:55:55 -0600

    grabbing user input and using post api method

--- app/public/surveyQ.js
+++ app/public/surveyQ.js
@@ -1,8 +1,9 @@
 var surveyQ = ["You look for opportunities for adventure in your life.", "You would rather have a cozy night in on a Friday night than a wild night out.", "You enjoy meeting new people.", "You would find it very difficult to spend 24hrs without access to social media.", "You like to spend a lot of time outdoors", "You have traveled to many different places and plan to visit many more.", "Your family is very important to you.", "You spend a lot of time daydreaming and fantasizing.", "You are very opinionated and outspoken.", "You learn best through experience and hands on activities."];
 
 for (var i = 0; i < surveyQ.length; i++) {
-  var qNum = $("<h3>Question #" + (i + 1) + "</h3>");
+  var count = i + 1;
+  var qNum = $("<h3>Question #" + count + "</h3>");
   var newQ = $("<p class='newQ'>" + surveyQ[i] + "</p>");
-  var newDropDown = '<div class="col-auto my-1"> <select class="custom-select mr-sm-2" id="inlineFormCustomSelect"> <option selected>Select and Option</option> <option value="1">1 (Strongly Disagree)</option> <option value="2">2</option> <option value="3">3</option> <option value="4">4</option> <option value="5">5 (Strongly Agree)</option> </select> </div>'
+  var newDropDown = '<div class="col-auto my-1"> <select class="custom-select survey-options mr-sm-2 ans' + count + '" id="inlineFormCustomSelect"> <option selected>Select an Option</option> <option value="1">1 (Strongly Disagree)</option> <option value="2">2</option> <option value="3">3</option> <option value="4">4</option> <option value="5">5 (Strongly Agree)</option> </select> </div>'
   $(".survey-questions").append(qNum, newQ, newDropDown);
 }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,6 +3,14 @@ var path = require("path");
 
 //load friend data
 var friendsArrData = require("../data/friends");
+console.log(friendsArrData);
+function compatible(object) {
+  for (var i = 0; i < friendsArrData.length; i++) {
+    for (var j = 0; j < object.scores.length; j++) {
+
+    }
+  }
+}
 
 module.exports = function (app) {
   //GET route to api/friends
@@ -12,7 +20,11 @@ module.exports = function (app) {
 
   //POST route to api/friends
   app.post("/api/friends", function (req, res) {
-    friendsArrData.push(req.body);
-    res.json(true);
+    if (req.body.scores.length === 10) {
+      compatible(req.body);
+      friendsArrData.push(req.body);
+      res.json(true);
+    }
+
   })
 }
